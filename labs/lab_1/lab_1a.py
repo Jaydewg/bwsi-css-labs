5699b11f00bc10dd157e4786cb87586a30ded69b
Jaydewg <jaydenwtao@outlook.com>
2026-03-10 22:00:49 -0500

    lie to the hypothetical teacher as a joke

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -5,7 +5,7 @@ The first lab in the BWSI CSS course. To complete this lab, fill out the variabl
 with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
 """
 
-# This is to simulate a remote editing robot's changes. edited_by_robot=True, i guess?
+# This is to simulate a remote editing robot's changes. edited_by_robot=AbsolutelyTrue, definitely
 
 
 def main():
